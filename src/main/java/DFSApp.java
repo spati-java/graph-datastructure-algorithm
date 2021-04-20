338f6443a85579d23f6971e894a8deafcfc5f7ce
Sourav Pati <sourav.pati09@gmail.com>
2021-04-20 09:31:21 -0500

    depth first search implementation

---
+++ src/main/java/DFSApp.java
@@ -0,0 +1,27 @@
+
+public class DFSApp {
+    public static void main(String[] args) {
+
+        Graph graph = new Graph();
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('E');
+        graph.addVertex('F');
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(0,3);
+        graph.addEdge(3,4);
+        graph.addEdge(4,5);
+        graph.addEdge(1,3);
+
+
+        System.out.println("Visits");
+
+        graph.depthFirstSearch(1);
+
+
+    }
+}
